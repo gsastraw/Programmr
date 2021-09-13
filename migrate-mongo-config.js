33eea60ef857f73158ce22d639771bd11b407056
gsastraw <gussasgr@student.gu.se>
2021-09-13 09:58:29 +0200

    Rename database name in migrate-mongo config

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -6,7 +6,7 @@ const config = {
     url: "mongodb://localhost:27017",
 
     // TODO Change this to your database name:
-    databaseName: "test",
+    databaseName: "programmr",
 
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
