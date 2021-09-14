36481369bb57641cb1b85d1be7ce893cb24f6b8a
gsastraw <gussasgr@student.gu.se>
2021-09-14 13:49:32 +0200

    Serialise and deserialise authenticated users

--- server/src/auth.js
+++ server/src/auth.js
@@ -4,6 +4,14 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GOOGLE_CLIENT_KEY = '1053310788215-dotkr3r02rmma7cdhvmutjf62ttpaj0n.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = 'xdO3ZMl-QHHmqSmOr1gIWXZs';
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+      done(null, user);
+});
+
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_KEY,
     clientSecret: GOOGLE_CLIENT_SECRET,
