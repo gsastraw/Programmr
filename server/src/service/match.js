eb920f0b41f5a6536f5cbfa62830b86cb323a7ba
Gregory Sastrawidjaya <gussasgr@student.gu.se>
2021-09-21 00:45:03 +0200

    Compare dislikes and likes field to determine a match or potential users to fetch

--- server/src/service/match.js
+++ server/src/service/match.js
@@ -6,25 +6,29 @@ const createMatch = async (userId, matchedId) => {
     /* When the suggested IDs are fetched from the match algorithm, this method first checks to see if the match already exists in the system.
        If the match already exists, a rejected Promise is returned. Otherwise, a Match is created that ties the user ID and the matched ID together.
     */
-    try {
-        const currentUser = await UserService.getUser(userId);
-        const matchedUser = await UserService.getUser(matchedId);
+    const currentUser = await UserService.getUser(userId);
+    const matchedUser = await UserService.getUser(matchedId);
 
-        if (!currentUser || !matchedUser) {
-            return Promise.reject(`Could not create a match between these two IDs!`)
-        }
+    if (!currentUser || !matchedUser) {
+        return Promise.reject(`Could not create a match between these two IDs!`)
+    }
 
-        if (checkIfMatchAlreadyExists(userId, matchedId)) {
-            return Promise.reject("Match was already created!");
+    for (let i = 0; i < currentUser.liked.length; i++) {
+        for (let j = 0; j < matchedUser.liked.length; j++) {
+            if (currentUser.liked[i] == matchedId) {
+                if (matchedUser.liked[j] == userId) {
+                    Match.create({ profiles: userId, matchedId });
+                }
+            } else {
+                return Promise.reject(`Two users need to match with each other in order to create a match!`)
+            }
         }
-    } catch (err) {
-        Match.create({ profiles: userId, matchedId });
     }
 }
 
 const checkIfMatchAlreadyExists = (userId, matchedId) => {
     // A helper function that returns true if the match.profiles contains the swiper's ID and the swipee's ID, else returns false
-    const match = Match.findOne( {  } ).exec();
+    const match = Match.find( {  } ).exec();
     return match.profiles.includes(userId, matchedId);
 }
 
