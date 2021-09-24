29d5bb2570a4a8eaff8b7242c0c42ae58d3bf949
gsastraw <gussasgr@student.gu.se>
2021-09-24 16:45:37 +0200

    Initial main menu commit

--- client/src/views/MainMenu.vue
+++ client/src/views/MainMenu.vue
@@ -1,15 +1,38 @@
 <template>
-<div>DA MAIN MENU IS ON HARRO!</div>
+<b-navbar toggleable="lg" fixed="top" type="dark" variant="dark">
+  <b-navbar-brand href="#">
+      <b-img class='logo' :src="require('../assets/squarelogo.svg')"></b-img>
+      Welcome
+  </b-navbar-brand>
+  <b-navbar-toggle target="nav-collapse">
+  </b-navbar-toggle>
+
+  <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav align="center">
+        <b-nav-item to='/mainmenu'>findMatch();</b-nav-item>
+        <b-nav-item href="/profile">myProfile();</b-nav-item>
+        <b-nav-item href="/matches">chat();</b-nav-item>
+        <b-nav-item href="/">signOut();</b-nav-item>
+      </b-navbar-nav>
+  </b-collapse>
+  </b-navbar>
+
 </template>
 
 <script>
 
 export default {
+  components: {
 
+  }
 }
 
 </script>
 
-<style>
-
+<style scope>
+.logo {
+    width: 15%;
+    align-items: center;
+    height: auto;
+}
 </style>
