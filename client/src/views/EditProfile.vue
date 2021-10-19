0afb16e7a15503d698f1bdb21566d662c67e2d89
Klara <klarasve@student.chalmers.se>
2021-10-19 20:08:52 +0200

    Implement PUT profile on frontend <3333

--- client/src/views/EditProfile.vue
+++ client/src/views/EditProfile.vue
@@ -5,6 +5,26 @@
     <edit-profile-item :userId=Number($route.params.id) />
     <b-button :to="{ name: 'profile', params: { userId: this.$route.params.userId }}" class="button" squared>Back</b-button>
     <b-button class="button" id="delete-user" squared v-on:click="deleteUser()">Delete account</b-button>
+    <b-button v-b-modal.modal-prevent-closing class="button">Reset profile</b-button>
+    <b-modal id="modal-prevent-closing" ref="modal" title="Create an account" hide-footer>
+      <form v-on:submit.prevent="createProfile">
+        <div class="form-group">
+            <label name="first-Name">First Name</label>
+            <input type="text" class="form-control" v-model="firstName" id="InputFirstName">
+          </div>
+          <div class="form-group">
+            <label name="last-Name">Last Name</label>
+            <input type="text" class="form-control" v-model="lastName" id="InputLastName">
+          </div>
+          <div class="form-group">
+            <label name="dob">Birthday </label>
+            <b-calendar v-model="dob" locale="en-US" id="InputDob"></b-calendar>
+          </div>
+          <div class="form-group">
+          <button class="btn btn-primary" id="create-button">Ok</button>
+        </div>
+      </form>
+    </b-modal>
 </div>
 </template>
 
@@ -21,6 +41,15 @@ export default {
     'edit-profile-item': EditProfileItem,
     Sidebar
   },
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      users: [],
+      context: null
+    }
+  },
   methods: {
     deleteUser() {
       const r = confirm('Are you sure you want to delete your account? This is irreversible.')
@@ -32,6 +61,21 @@ export default {
             alert(error)
           })
         window.location.href = '/'
+      }
+    },
+    createProfile() {
+      const r = confirm('Are you sure you want to create a new profile? This is irreversible and will overwrite your current profile.')
+      if (r === true) {
+        Api.put('/users/' + this.$route.params.userId + '/profile', { firstName: this.firstName, lastName: this.lastName, dob: this.dob })
+          .then(response => {
+            console.log(response)
+            alert('Profile has been created, sign in again to see your profile')
+            window.location.href = '/'
+          })
+          .catch(error => {
+            alert('Profile could not be created')
+            console.log(error)
+          })
       }
     }
   }
